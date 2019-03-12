d9965f1cd175233e5a1ba45e3bf97553c45c6a59
UserXGnu <user_x@riseup.net>
2019-03-12 22:51:35 +0000

    Add VPN support

--- .dwm/dwmstatus_custom/dwmstatus.c
+++ .dwm/dwmstatus_custom/dwmstatus.c
@@ -15,7 +15,7 @@
 typedef enum {
   DOWN,
   UP
-} wifi_status_t;
+} status_t;
 
 typedef enum {
   NONE,
@@ -60,7 +60,7 @@ static battery_status_t fread_battery_status(const char *filename){
   }
 }
 
-static wifi_status_t fread_wifi_status(const char *filename){
+static status_t fread_wifi_status(const char *filename){
   char buf[16];
   FILE *file = fopen(filename, "r");
   if(!file) {
@@ -96,7 +96,7 @@ static int battery_get_percent(battery_info_t bat) {
 
 static int fetch_wifi_info(char *wifi_str) {
   int len = 0;
-  wifi_status_t wifi = fread_wifi_status("/sys/class/net/wlp3s0/operstate");
+  status_t wifi = fread_wifi_status("/sys/class/net/wlp4s0/operstate");
 
   if(wifi == UP) {
     len += snprintf(wifi_str, STRSZ, "\ue220");
@@ -145,24 +145,56 @@ static int fetch_current_time(char *time_str) {
   return strftime(time_str, STRSZ, "%a %d, %b %H:%M %p \ue0a5 ", current_tm);
 }
 
+#include <net/if.h>
+static status_t fetch_vpn_status (void) {
+    const char * vpn_ifaces[] = { "tap", "tun", NULL };
+    int nifaces;
+    status_t status = DOWN;
+    for (nifaces = 0; vpn_ifaces[nifaces]; nifaces++);
+
+    struct if_nameindex * iface_indexes,
+                        * iface_auxiliar;
+
+    iface_indexes = if_nameindex();
+    if ( iface_indexes) {
+        for (iface_auxiliar = iface_indexes;
+                iface_auxiliar->if_index || iface_auxiliar->if_name;
+                iface_auxiliar++) {
+            for (int i = 0; i < nifaces; i++) {
+                if (strstr (iface_auxiliar->if_name, vpn_ifaces[i])) { status = UP; break; }
+            }
+        }
+    }
+    if_freenameindex(iface_indexes);
+    return status;
+}
+
+static void fetch_vpn_info (char * vpnstatus) {
+    memset(vpnstatus, 0x00, STRSZ);
+
+    snprintf (vpnstatus, STRSZ, (fetch_vpn_status() == UP ? "\ue1c2" : "\ue1bc"));
+}
+
 int main(void) {
   static Display *dpy;
   char status[STATUSSZ];
   char battery[STRSZ];
   char wifi[STRSZ];
   char time[STRSZ];
+  char vpnstatus[STRSZ];
 
   if (!(dpy = XOpenDisplay(NULL))) {
     fprintf(stderr, "dwmstatus: cannot open display.\n");
     return 1;
   }
 
-  for (;;sleep(20)) {
+  for (;;sleep(5)) {
     fetch_wifi_info(wifi);
     fetch_battery_info(battery);
     fetch_current_time(time);
+    fetch_vpn_info (vpnstatus);
 
-    snprintf(status, STATUSSZ, "%s %s %s", wifi, battery, time);
+    snprintf(status, STATUSSZ, "%s %s %s %s", vpnstatus, wifi, battery, time);
 
     XStoreName(dpy, DefaultRootWindow(dpy), status);
     XFlush(dpy);
